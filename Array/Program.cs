93f31429000e6202d07ea0b3ede59d9bbb4a8234
Elene <L.Rokolunskaya@Gmail.com>
2022-08-06 20:24:40 +0300

    добавила array обновленный

---
+++ Array/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] Firstarray=new string[5] {"Love", "25", "13", "mur", "*-*"};
+string[] Secondarray= new string[Firstarray.Length];
+void Apgrarray(string[] Firstarray, string[]Secondarray)
+{
+    int current=0;
+    for (int i = 0; i < Firstarray.Length; i++)
+    {
+      if (Firstarray[i].Length<4)
+      {
+        Secondarray[current]= Firstarray[i];
+        current++;
+      }  
+    }
+}
+void Showarray(string[]array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+       Console.Write($"{array[i]}"); 
+    }
+    Console.WriteLine();
+}
+Apgrarray(Firstarray, Secondarray);
+Showarray(Secondarray);
